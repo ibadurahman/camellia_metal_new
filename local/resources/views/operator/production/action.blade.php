60d1345271104566529ad68372879c570cd522cb
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2023-04-04 14:02:31 +0700

    update 040423 sidebar view

--- local/resources/views/operator/production/action.blade.php
+++ local/resources/views/operator/production/action.blade.php
@@ -1,7 +1,7 @@
 @if ($model->machine->ip_address == request()->ip())
     <a href="{{ url('operator/production/' . $model->id . '/show') }}" class="btn btn-primary">Go To Report Page</a>
 @else
-    @if (auth()->user()->hasRole('supervisor'))
+    @if (auth()->user()->hasRole('supervisor|super-admin|owner'))
         <a href="{{ url('operator/production/' . $model->id . '/show') }}" class="btn btn-success">Supervise</a>
     @else
         <span class="text-danger">you have no rights to process this workorder</span>
