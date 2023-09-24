34ecb407b43c7e432b5e0750ad08e54f03a2b89c
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2023-09-25 01:11:46 +0700

    fix machine list on supervisor

--- local/resources/views/operator/production/index.blade.php
+++ local/resources/views/operator/production/index.blade.php
@@ -17,9 +17,8 @@
                                     <select name="" id="machine-selector" onchange="update_sorting()"
                                         class="form-control">
                                         @foreach ($machines as $machine)
-                                            @if ($machine->ip_address == request()->ip()) 
-                                                <option value="{{ $machine->id }}"
-                                                    selected>{{ $machine->name }}
+                                            @if ($machine->ip_address == request()->ip() || auth()->user->hasRole('supervisor'))
+                                                <option value="{{ $machine->id }}" selected>{{ $machine->name }}
                                                 </option>
                                             @endif
                                         @endforeach
