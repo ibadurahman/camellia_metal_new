a1f8c488e09ef448c6eb0b41ba42abb71155eb53
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2024-04-23 16:06:15 +0700

    update pagination downtime

--- local/app/Http/Controllers/DowntimeController.php
+++ local/app/Http/Controllers/DowntimeController.php
@@ -19,7 +19,7 @@ class DowntimeController extends Controller
         $downtimeData = Downtime::where('workorder_id',$request->workorder_id)
                             ->where('status','stop')
                             ->orderBy('is_remark_filled','asc')
-                            ->orderby('id','desc')->get();
+                            ->orderby('id','desc')->paginate(100);
         return new DowntimeCollection($downtimeData);
     }
 
