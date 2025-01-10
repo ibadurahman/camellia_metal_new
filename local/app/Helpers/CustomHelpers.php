70ed34ac5fd3e6d9d7f420e77c77a7a5ee6ee7b2
Ibadurahman Hanif <ibadurahmanhanif@gmail.com>
2025-01-10 13:51:53 +0700

    create custom helpers file

---
+++ local/app/Helpers/CustomHelpers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers;
+
+class CustomHelpers
+{
+    public static function isContentValid($content)
+    {
+        if(!$content || trim($content) == '' || $content == 'null' || $content == 'undefined' || $content == 'NaN' || trim($content) == '-'){
+            return false;
+        }
+        return true;
+    }
+}
